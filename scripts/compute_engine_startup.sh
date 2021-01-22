4d9de0f6e595f5f2146e8a0b8965d38698e614d7
Abilash <abilashr@thoughtworks.com>
2021-01-22 17:23:17 +0530

    Updated jar download file path to git

--- scripts/compute_engine_startup.sh
+++ scripts/compute_engine_startup.sh
@@ -1,5 +1,5 @@
 ! /bin/bash
 sudo add-apt-repository ppa:openjdk-r/ppa -y && sudo apt-get update -y
 sudo apt install openjdk-11-jdk -y
-gsutil cp gs://demo-jars/VERSION_1/demo-0.0.1-SNAPSHOT.jar .
+wget https://github.com/abilaashsai/GCP-Workshop/releases/download/1/demo-0.0.1-SNAPSHOT.jar
 java -jar demo-0.0.1-SNAPSHOT.jar
