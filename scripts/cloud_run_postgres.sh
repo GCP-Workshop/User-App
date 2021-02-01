9f2e357f282235d026386f9efc8b93b521cc3ac8
Abilash <abilashr@thoughtworks.com>
2021-02-01 12:15:08 +0530

    Updated cloud run script

--- scripts/cloud_run_postgres.sh
+++ scripts/cloud_run_postgres.sh
@@ -1,7 +1,6 @@
 gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun:2 \
   --platform managed \
   --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com \
-  --image eu.gcr.io/otto-university-302315/demorun:3 \
   --update-env-vars POSTGRES_HOST=localhost,POSTGRES_USERNAME=postgres,POSTGRES_PASS=admin
   --region europe-west3 \
   --allow-unauthenticated
