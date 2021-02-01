0308a58956c5bf0d81b05a8701601c117b6ce572
Abilash <abilashr@thoughtworks.com>
2021-02-01 11:37:42 +0530

    Added cloud run deployment scripts with service account creation

--- scripts/cloud_run.sh
+++ scripts/cloud_run.sh
@@ -1,1 +1,11 @@
-gcloud run deploy --image eu.gcr.io/otto-university-302315/demorun:2 --platform managed --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com
+gcloud iam service-accounts create cloud-run-acc \
+    --description="Cloud run service account" \
+    --display-name="cloud-run-acc"
+gcloud projects add-iam-policy-binding otto-university-302315 \
+  --member="serviceAccount:cloud-run-acc@otto-university-302315.iam.gserviceaccount.com" \
+  --role="roles/run.invoker"
+gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun:2 \
+  --platform managed \
+  --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com \
+  --region europe-west3 \
+  --allow-unauthenticated
