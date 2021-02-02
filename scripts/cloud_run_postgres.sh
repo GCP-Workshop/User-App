cdad2d28fdf0392ad16ec4e976764bca2a2a39e7
dhevasenapathi <dhevasenapathi@dhevaseathisMBP.domain.name>
2021-02-02 15:47:02 +0530

    Database creation through cli script added

--- scripts/cloud_run_postgres.sh
+++ scripts/cloud_run_postgres.sh
@@ -1,6 +1,12 @@
-gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun:2 \
+ gcloud --project=otto-university-302315 beta sql instances create initdb --region=europe-west3 --network=test-vpc --no-assign-ip
+gcloud sql databases create users --instance=initdb
+export DB_IP=$(gcloud sql instances describe initdb --project otto-university-302315 --format 'value(ipAddresses.ipAddress)')
+
+gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun3:2 \
   --platform managed \
   --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com \
-  --update-env-vars POSTGRES_HOST=localhost,POSTGRES_USERNAME=postgres,POSTGRES_PASS=admin
+  --vpc-connector test-vpc \
+  --add-cloudsql-instances otto-university-302315:europe-west3:testdb \
+  --update-env-vars POSTGRES_HOST=localhost,POSTGRES_USERNAME=postgres,POSTGRES_PASS=admin \
   --region europe-west3 \
   --allow-unauthenticated
