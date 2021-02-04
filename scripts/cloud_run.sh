30466b7d1f5dfe706b0b6b38fd31487dff025d69
dhevasenapathi <dhevasenapathi.s@gmail.com>
2021-02-04 14:30:52 +0530

    cloud build with cloud run scripts added

--- scripts/cloud_run.sh
+++ scripts/cloud_run.sh
@@ -4,8 +4,10 @@ gcloud iam service-accounts create cloud-run-acc \
 gcloud projects add-iam-policy-binding otto-university-302315 \
   --member="serviceAccount:cloud-run-acc@otto-university-302315.iam.gserviceaccount.com" \
   --role="roles/run.invoker"
-gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun:2 \
+gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun3:7 \
   --platform managed \
   --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com \
+  --vpc-connector vpcconnector1 \
+  --add-cloudsql-instances second \
   --region europe-west3 \
   --allow-unauthenticated
--- scripts/cloud_run_postgres.sh
+++ scripts/cloud_run_postgres.sh
@@ -1,12 +1,11 @@
-gcloud --project=otto-university-302315 beta sql instances create initdb --region=europe-west3 --network=test-vpc --no-assign-ip
-gcloud sql databases create users --instance=initdb
-export DB_IP=$(gcloud sql instances describe initdb --project otto-university-302315 --format 'value(ipAddresses.ipAddress)')
-
-gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun3:2 \
+gcloud --project=otto-university-302315 beta sql instances create thirddb  --database-version=POSTGRES_12 --cpu=2 --memory=10GB  --region=europe-west3 --network=test-vpc --no-assign-ip
+gcloud sql databases create users --instance=thirddb
+export DB_IP=$(gcloud sql instances describe thirddb --project otto-university-302315 --format 'value(ipAddresses.ipAddress)')
+gcloud sql users set-password postgres --instance=thirddb --prompt-for-password
+gcloud run deploy user-app --image eu.gcr.io/otto-university-302315/demorun3:9 \
   --platform managed \
   --service-account cloud-run-acc@otto-university-302315.iam.gserviceaccount.com \
   --vpc-connector vpcconnector1 \
-  --add-cloudsql-instances otto-university-302315:europe-west3:initdb \
-  --update-env-vars POSTGRES_HOST=10.179.50.3,POSTGRES_USERNAME=postgres,POSTGRES_PASS=test@123 \
+  --add-cloudsql-instances otto-university-302315:europe-west3:thirddb \
   --region europe-west3 \
   --allow-unauthenticated
